96ad7c9418ca410ea6366b36a4ead1ed36317c62
James Matchett <jamesm@kainos.com>
2019-10-31 16:24:48 +0000

    Add API calls to DB for getting/adding courses

--- ExpressAPI/db.js
+++ ExpressAPI/db.js
@@ -10,4 +10,21 @@ const db = mysql.createConnection({
 db.connect(function(err){
     if(err) throw err;
     console.log("Connected to MySql");
-})
+});
+
+exports.addCourse = function(data, readyFn){
+    db.query('INSERT INTO course ?', data,
+    function(error, results, fields){
+        if(error) throw error;
+        readyFn(results.insertId);
+    });
+}
+
+exports.getCourses = function(callback){
+    db.query(
+        "SELECT courseID, courseName, courseDate, location, description, courseTrainer, info "
+        +"FROM course", function(err,rows){
+            if(err) throw err;
+            callback(rows);
+        });
+      }
