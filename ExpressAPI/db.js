f127052914fecd0d3d36faa9382c1e478561724d
James Matchett <jamesm@kainos.com>
2019-11-01 10:10:31 +0000

    Add edit course API

--- ExpressAPI/db.js
+++ ExpressAPI/db.js
@@ -11,3 +11,13 @@ db.connect(function(err){
     if(err) throw err;
     console.log("Connected to MySql");
 })
+
+exports.editCourse = function(data, readyFn){
+    db.query('UPDATE course set courseName = ?, courseDate = ?, location = ?, '+ 
+    "description = ?, courseTrainer = ?, info = ? ",
+    [data.courseName, data.courseDate, data.location, data.description, data.courseTrainer, data.info],
+    function(error, results, fields){
+        if(error) throw error;
+        readyFn(results.insertId);
+    });
+}
