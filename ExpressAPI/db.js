183367cff7bda8e6969c5b974442622febc8d02b
James Matchett <jamesm@kainos.com>
2019-11-01 12:23:39 +0000

    Add delete route for api

--- ExpressAPI/db.js
+++ ExpressAPI/db.js
@@ -11,3 +11,42 @@ db.connect(function(err){
     if(err) throw err;
     console.log("Connected to MySql");
 })
+
+
+exports.addCourse = function(data, readyFn){
+    db.query('INSERT INTO course SET ?', data,
+    function(error, results, fields){
+        if(error) throw error;
+        readyFn(results.insertId);
+    });
+}
+
+exports.getCourses = function(callback){
+    db.query(
+        "SELECT courseID, courseName, courseDate, location, description, courseTrainer, info "
+        +"FROM course", function(err,rows){
+            if(err) throw err;
+            callback(rows);
+        });
+      }
+
+exports.editCourse = function(data, readyFn){
+
+    console.log(data.courseName, data.courseDate, data.location, data.description, data.courseTrainer, data.info, data.courseID)
+    
+    db.query('UPDATE course set courseName = ?, courseDate = ?, location = ?, '+ 
+    "description = ?, courseTrainer = ?, info = ? where courseID = ?",
+    [data.courseName, data.courseDate, data.location, data.description, data.courseTrainer, data.info, data.courseID],
+    function(error, results, fields){
+        if(error) throw error;
+        readyFn(results.insertId);
+    });
+}
+
+exports.deleteCourse = function(data, readyFn){
+    db.query('DELETE FROM course where courseID = ?', data.courseID,
+    function(error, results, fields){
+        if(error) throw error;
+        readyFn(results.insertId);
+    });
+}
