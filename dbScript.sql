81e7a7844a9a663449c5641c114836c5256339fe
Jessica McMillen <jessicam@kainos.com>
2019-11-01 09:19:42 +0000

    Add branding
    Index update
    
    Init Angular app
    
    Update angular.json to use port 8000
    
    Add Init express api
    
    First pass of db script
    
    Updated db script
    
    Updating date name
    
    Add proxy to connect frontend to DB
    
    Added dummy data
    
    Updating dummy data

---
+++ dbScript.sql
@@ -0,0 +1,93 @@
+DROP SCHEMA IF EXISTS training;
+CREATE SCHEMA training;
+USE training;
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee(
+ID INT(4) NOT NULL AUTO_INCREMENT,
+firstName VARCHAR(30) NOT NULL,
+lastName VARCHAR(30) NOT NULL,
+email VARCHAR (50) NOT NULL,
+managerEmail VARCHAR(50) NOT NULL,
+PRIMARY KEY (ID)
+);
+
+DROP TABLE IF EXISTS course;
+
+CREATE TABLE course(
+courseID INT(4) NOT NULL AUTO_INCREMENT,
+courseName VARCHAR(100) NOT NULL,
+courseDate DATE NOT NULL,
+location VARCHAR(50) NOT NULL,
+description VARCHAR(255) NOT NULL,
+courseTrainer VARCHAR(100) NOT NULL,
+info VARCHAR (200) NOT NULL,
+PRIMARY KEY (courseID)
+);
+
+DROP TABLE IF EXISTS courseEmployee;
+
+CREATE TABLE courseEmployee(
+courseID int(4) NOT NULL,
+employeeID int(4) NOT NULL,
+isWaiting BIT NOT NULL DEFAULT 0,
+isAttend BIT NOT NULL DEFAULT 0
+);
+
+ALTER TABLE courseEmployee
+ADD FOREIGN KEY (employeeID) REFERENCES employee(ID);
+
+ALTER TABLE courseEmployee
+ADD FOREIGN KEY (courseID) REFERENCES course(courseID);
+
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Chris', 'Thompson', 'chris.thompson@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Gordon', 'Kelly', 'g.kelly@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Maeve', 'Donnelly', 'm.donnelly@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Jessica', 'McMillen', 'j.mcmillen@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Joshua', 'Higginson', 'j.higginson@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Eoin', 'McCoy', 'e.mccoy@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('James', 'Matchett', 'j.matchett@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Conor', 'Moss', 'c.moss@kainos.com', 't.boyle@kainos.com');
+insert into employee (firstName, lastName, email, managerEmail)
+values ('Ryan', 'Connolly', 'r.connolly@kainos.com', 't.boyle@kainos.com');
+
+insert into course (courseName, courseDate, location, description, courseTrainer, info)
+values ('EAYL Induction', date'2019-12-14', 'Park Inn', 'Getting new EAYLs up to speed with company processes and basic programing languages.', 'Shaun Ganley', 'Suitable for those who have recently joined Kainos as EAYL');
+insert into course (courseName, courseDate, location, description, courseTrainer, info)
+values ('Trainee Development Programme', date'2019-11-03', 'Mount Charles', 'Getting placements and graduates up to speed with company processes and basic programing languages.', 'Roisin Goodman', 'Suitable for those who have recently joined Kainos as a graduate or placement student.');
+insert into course (courseName, courseDate, location, description, courseTrainer, info)
+values ('New Manager Crash Course', date'2020-03-23', 'Ascot', 'Teaching hopeful managers how to rule over their subordinates', 'Brendan Mooney', 'Suitable for those who want to earn more of that sweet dolla.');
+insert into course (courseName, courseDate, location, description, courseTrainer, info)
+values ('Taking It Easy', date'2020-06-27', 'Upper Crescent', 'Helping you rest and relax in the workplace', 'Ryan Connolly', 'Suitable for those who want People Support to start secretly investigating them.');
+
+insert into courseEmployee(courseID, employeeID)
+values (1, 1);
+insert into courseEmployee(courseID, employeeID)
+values (1, 2);
+insert into courseEmployee(courseID, employeeID)
+values (1, 3);
+insert into courseEmployee(courseID, employeeID)
+values (2, 4);
+insert into courseEmployee(courseID, employeeID)
+values (2, 5);
+insert into courseEmployee(courseID, employeeID)
+values (2, 6);
+insert into courseEmployee(courseID, employeeID)
+values (3, 7);
+insert into courseEmployee(courseID, employeeID)
+values (3, 8);
+insert into courseEmployee(courseID, employeeID)
+values (4, 9);
+
+
+Select course.courseID, courseName, courseDate, location, description, courseTrainer from course;
+
--- training-hub/src/app/app.component.ts
+++ training-hub/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'training-root',
+  selector: 'training-hub-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
