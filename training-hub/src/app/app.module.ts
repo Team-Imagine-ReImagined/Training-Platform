4f3cfac4081367325d7e8d58746ed160b4a4cc7f
Jessica <j.mcmillen@kainos.com>
2019-11-01 15:29:28 +0000

    Linking

--- training-hub/src/app/app.module.ts
+++ training-hub/src/app/app.module.ts
@@ -5,19 +5,37 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { AppComponent } from './app.component'
 import { CourseListComponent } from './course-list/course-list.component'
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'test', component: TestComponent },
+  { path: '', redirectTo: 'course', pathMatch: 'full'},
+  { path: 'course', component: CourseListComponent },
+  { path: '', redirectTo: 'course', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
     NgbModule,
     BrowserModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- training-hub/src/app/course-list/course-list.component.ts
+++ training-hub/src/app/course-list/course-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core'
 import { DataService } from '../data.service'
 
 @Component({
-  selector: 'training-hub-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.css']
 })
